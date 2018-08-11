736a6c7aaba63167091ef6f213bf394c901f5277
Alessandro Santospirito <alessandro.santospirito@gmx.de>
2018-08-12 00:06:46 +0200

    Setting up the project and adding vuetify

---
+++ src/components/Todo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Todo</h1>
+    <v-card>
+      Here can be Todo-Tasks
+      <v-icon
+        color="green">
+        add 
+      </v-icon>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
